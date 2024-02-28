e7452d732cbc0f4be2c18d856a30f636ce55261d
smallmoon2 <106749904+smallmoon2@users.noreply.github.com>
2024-02-29 02:04:02 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -5,12 +5,13 @@
 using namespace std;
 
 int solution(vector<int> citations) {
-    int answer = 0;
     sort(citations.begin(), citations.end(), greater<int>());
-    if (!citations[0])        return 0;
-    for (int i = 0; i < citations.size(); i++)    {
-        if (citations[i] > i)    answer++;
-        else    break;
+
+    for (int i = 0; i < citations.size(); ++i) {
+        if (citations[i] < i + 1) {
+            return i;
+        }
     }
-    return answer;
+
+    return citations.size();
 }
