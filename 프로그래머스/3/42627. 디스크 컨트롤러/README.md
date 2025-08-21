54bcf3768970786410c943411d929b1c1a9cf9ef
smallmoon2 <106749904+smallmoon2@users.noreply.github.com>
2025-08-22 01:02:39 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.45 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627#) 
 
 ### 성능 요약
 
-메모리: 31.3 MB, 시간: 4.67 ms
+메모리: 31.3 MB, 시간: 4.45 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 22일 00:59:39
+2025년 08월 22일 01:02:37
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cs
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cs
@@ -6,32 +6,27 @@ public class Solution {
         int answer = 0;
         int time = 0;
         int totaltime = 0;
-        // 	 〉	(3, 0) (6, 2) (9, 1)
-        
+
         var job = new List<(int, int)>();
         for(int i =0; i < jobs.GetLength(0); i++){
             job.Add((jobs[i,1],jobs[i,0]));
         }
-        
         job.Sort();
-        //Console.WriteLine(job[0].Item1);
         
         while(job.Count > 0){
             
             for(int j = 0; j < job.Count; j++){
-                if(time >= job[j].Item2){
+                int playtime = job[j].Item1;
+                int start = job[j].Item2;
                     
+                if(time >= job[j].Item2){
                     totaltime = totaltime + job[j].Item1 + time - job[j].Item2;
                     time = time + job[j].Item1 - 1;
                     job.RemoveAt(j);
                     break;
                 }
             }
-            //foreach (var item in job) {
-                //Console.WriteLine(item);
-            //}
             time ++;
-            //Console.WriteLine(time);
         }
 
         return totaltime/jobs.GetLength(0);;
